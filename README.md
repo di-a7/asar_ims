d8a7d748ad5704eb2e47c00edd085468af8ede4f
diya <shresthadiya59@gmail.com>
2025-08-20 16:06:59 +0545

    model creation

---
+++ README.md
@@ -0,0 +1,12 @@
+# Inventory Management System
+
+# Installation
+- python -m venv env
+
+- pip install -r requirements.txt
+
+- python manage.py migrate
+
+- python manage.py createsuperuser(admin)
+
+- python manage.py runserver
---
+++ ims/models.py
@@ -0,0 +1,34 @@
+from django.db import models
+
+# Create your models here.
+class Department(models.Model):
+   name = models.CharField(max_length=100)
+
+class ProductCategory(models.Model):
+   name = models.CharField(max_length=50)
+
+class Products(models.Model):
+   name = models.CharField(max_length=100)
+   description = models.TextField()
+   stock = models.IntegerField()
+   category = models.ForeignKey(ProductCategory, on_delete=models.CASCADE)
+   department = models.ForeignKey(Department, on_delete=models.CASCADE)
+
+class Supplier(models.Model):
+   AVAILABLE = 'A'
+   NOT_AVAILABLE = "N"
+   CHOICES = {
+      AVAILABLE : "Available",
+      NOT_AVAILABLE : "Not Available"
+   }
+   name = models.CharField(max_length=100) 
+   is_available = models.CharField(max_length=1, choices=CHOICES, default=AVAILABLE)
+
+class Purchase(models.Model):
+   quantity = models.IntegerField()
+   price = models.FloatField()
+   supplier = models.ForeignKey(Supplier, on_delete=models.CASCADE)
+
+class PurchaseItem(models.Model):
+   product = models.ForeignKey(Products, on_delete=models.PROTECT)
+   purchase = models.ForeignKey(Purchase, on_delete=models.PROTECT)
